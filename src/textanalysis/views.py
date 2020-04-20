14348158ab7ff533dd8e91978e35cf59b32a054a
crueldad <ian.crueldad@cgu.edu>
2020-04-20 13:07:31 -0700

    uploading changes

--- src/textanalysis/views.py
+++ src/textanalysis/views.py
@@ -85,7 +85,6 @@ def gettext(request):
 
     
     for x in parsed_dictionary:
-        urlz = 'https://i.ibb.co/RPb5QPm/rsz-fa3.jpg'
         if x == 'FISHES SWIMMING':
             FSV = parsed_dictionary['FISHES SWIMMING']
             K = []
@@ -105,7 +104,6 @@ def gettext(request):
             y = F
             
         if x == 'NAUTICAL WONDER':
-            urlz = 'https://i.ibb.co/Xp2rdwp/rsz-fa2.jpg'
             FSV1 = parsed_dictionary['NAUTICAL WONDER']
             Z = []
             for sentence1 in FSV1:
@@ -124,7 +122,6 @@ def gettext(request):
             y = N
             
         if x == 'DESERT BIRD':
-            urlz = 'https://i.ibb.co/CHQdnYC/rsz-fa1.jpg'
             FSV2 = parsed_dictionary['DESERT BIRD']
             A = []
             for sentence2 in FSV2:
@@ -184,130 +181,18 @@ def gettext(request):
         x = int(x)
     most_common_w = 'These are the common words from all comments under the choosen artwork:', (fdist.most_common(x)) 
 
-    parsed_dictionarys = "All the comments for all artwork submitted: ", parsed_dictionary['DESERT BIRD']
+    
     DB = parsed_dictionary['DESERT BIRD'] 
-    dbs = str(DB)
-    dbss = dbs.replace("]"," ").replace("[","").replace("'","")
+
     NW = parsed_dictionary['NAUTICAL WONDER']
+
     SW = parsed_dictionary['FISHES SWIMMING']
+
     
 
 
         
     return render(request, 'textanalysis/textanalysis.html', {'Best_Comment':Best_Comment, 'sentiment':sentiment,\
-        'most_common_w':most_common_w, 'dbss':dbss, 'NW':NW, 'SW':SW, 'urlz':urlz, 'F':F, 'N':N, 'D':D})
+        'most_common_w':most_common_w, 'DB':DB, 'NW':NW, 'SW':SW, 'F':F, 'N':N, 'D':D})
     
    
-
-    
-
-
-        
-        # CI = k.replace("'", "").replace("{", "").replace("}", "").replace(","," ----").replace("Comment_Box", "Comment")
-        # most_common = (fdist.most_common(5)) + ["--Top 5 Common Words"]
-        # SentimentAnalysis = [ress] + ["--Sentiment Analysis"]
-
-        
-            
-
-        # blob_ = TextBlob(text)
-        # blob = word_tokenize(text)
-        # stop_words= set(stopwords.words('english'))
-        # filtered_sent= []
-        # for w in blob:
-        #     if w not in stop_words:
-        #         filtered_sent.append(w)
-
-        # vader = SentimentIntensityAnalyzer()
-        # y = vader.polarity_scores(text)
-        # fdist = FreqDist(filtered_sent)
-        # if -1<= y['compound'] < -0.6:
-        #     res = ('Negative')
-        # if -.6<= y['compound'] < -0.2:
-        #     res = ('Somewhat Negative')
-        # if -0.2 <= y['compound'] < 0.2:
-        #     res = ('Neutral')
-        # if 0.2 <= y['compound'] < .6:
-        #     res = ('Somewhat Positive')
-        # if .6 <= y['compound'] <= 1.0:
-        #     res = ('Positive')
-        # CI = k.replace("'", "").replace("{", "").replace("}", "").replace(","," ----").replace("Comment_Box", "Comment")
-        # most_common = (fdist.most_common(5)) + ["--Top 5 Common Words"]
-        # SentimentAnalysis = [res] + ["--Sentiment Analysis"]
-
-        # return render(request, 'textanalysis/textanalysis.html', {'CI':CI, 'most_common':most_common, 'SentimentAnalysis':SentimentAnalysis })
-
-            
-    # for i in info:
-    #     k = str(i)
-    #     diction = eval(k)
-    #     text = diction.get('Comment_Box')
-    #     tb = TextBlob(text)
-
-    #     if tb.detect_language() == 'en':
-    #         tb = text
-    #     else:
-    #         tb.translate(to='en')
-    #         egv = tb.translate(to='en')
-    #         translation = tb
-    #         text = tb
-            
-    #         translation = tb
-    #         evgg = str(egv)
-    #         englishversion = evgg.replace("TextBlob", "").replace(".", "").replace("!","").replace("?","")
-    #         iftranslation = [englishversion] +["----English translation"]
-
-    #         blob_ = TextBlob(englishversion)
-    #         blob = word_tokenize(englishversion)
-    #         stop_words= set(stopwords.words('english'))
-    #         filtered_sent= []
-    #         for w in blob:
-    #             if w not in stop_words:
-    #                 filtered_sent.append(w)
-
-    #         vader = SentimentIntensityAnalyzer()
-    #         y = vader.polarity_scores(englishversion)
-    #         fdist = FreqDist(filtered_sent)
-    #         if -1<= y['compound'] < -0.6:
-    #             res = ('Negative')
-    #         if -.6<= y['compound'] < -0.2:
-    #             res = ('Somewhat Negative')
-    #         if -0.2 <= y['compound'] < 0.2:
-    #             res = ('Neutral')
-    #         if 0.2 <= y['compound'] < .6:
-    #             res = ('Somewhat Positive')
-    #         if .6 <= y['compound'] <= 1.0:
-    #             res = ('Positive')
-    #         CI = k.replace("'", "").replace("{", "").replace("}", "").replace(","," ----").replace("Comment_Box", "Comment")
-    #         most_common = (fdist.most_common(5)) + ["--Top 5 Common Words"]
-    #         SentimentAnalysis = [res] + ["--Sentiment Analysis"]
-
-            # return render(request, 'textanalysis/textanalysis.html', {'most_common': most_common, "CI":CI, "SentimentAnalysis":SentimentAnalysis, "iftranslation":iftranslation})
-        
-
-        # blob_ = TextBlob(text)
-        # blob = word_tokenize(text)
-        # stop_words= set(stopwords.words('english'))
-        # filtered_sent= []
-        # for w in blob:
-        #     if w not in stop_words:
-        #         filtered_sent.append(w)
-
-        # vader = SentimentIntensityAnalyzer()
-        # y = vader.polarity_scores(text)
-        # fdist = FreqDist(filtered_sent)
-        # if -1<= y['compound'] < -0.6:
-        #     res = ('Negative')
-        # if -.6<= y['compound'] < -0.2:
-        #     res = ('Somewhat Negative')
-        # if -0.2 <= y['compound'] < 0.2:
-        #     res = ('Neutral')
-        # if 0.2 <= y['compound'] < .6:
-        #     res = ('Somewhat Positive')
-        # if .6 <= y['compound'] <= 1.0:
-        #     res = ('Positive')
-        # CI = k.replace("'", "").replace("{", "").replace("}", "").replace(","," ----").replace("Comment_Box", "Comment")
-        # most_common = (fdist.most_common(5)) + ["--Top 5 Common Words"]
-        # SentimentAnalysis = [res] + ["--Sentiment Analysis"]
-        
-        # return render(request, 'textanalysis/textanalysis.html', {'most_common': most_common, "CI":CI, "SentimentAnalysis":SentimentAnalysis})
